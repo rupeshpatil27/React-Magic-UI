d881ceb45c637c80df40729204b176addcd4d24a
rupeshpatil <rupeshpatil88927@gmail.com>
2025-08-31 23:39:35 +0530

    initialize setup for Orbiting Circles magic ui

---
+++ orbiting-circles/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="relative w-full h-screen">
+      <h1 className="text-center mt-10 text-3xl font-bold">Orbiting Circles</h1>
+    </div>
+  )
+}
+
+export default App
