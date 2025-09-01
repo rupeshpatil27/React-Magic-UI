345b00cab2125b5cf004b5943777a7bad01d0062
rupeshpatil <rupeshpatil88927@gmail.com>
2025-09-01 23:46:00 +0530

    start working on icon cloud magic ui component

---
+++ icon-cloud/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="relative">
+      <h1 className="text-center mt-10 text-3xl font-bold">Icon Cloud</h1>
+
+    </div>
+  );
+}
+
+export default App;
