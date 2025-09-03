ccb6f79cacc8e50c1938617209ebcf66ac2dd696
rupeshpatil <rupeshpatil88927@gmail.com>
2025-09-03 23:57:51 +0530

    add bento grid component

---
+++ bento-grid/src/App.jsx
@@ -0,0 +1,67 @@
+
+import { FiFileText } from "react-icons/fi";
+import { MdInput } from "react-icons/md";
+import { CiGlobe } from "react-icons/ci";
+import { BentoCard, BentoGrid } from "./component/BentoGrid";
+
+
+function App() {
+
+  const features = [
+    {
+      Icon: FiFileText,
+      name: "Save your files",
+      description: "We automatically save your files as you type.",
+      href: "/",
+      cta: "Learn more",
+      background: <img className="absolute -right-20 -top-20 opacity-60" />,
+      className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
+    },
+    {
+      Icon: MdInput,
+      name: "Full text search",
+      description: "Search through all your files in one place.",
+      href: "/",
+      cta: "Learn more",
+      background: <img className="absolute -right-20 -top-20 opacity-60" />,
+      className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
+    },
+    {
+      Icon: CiGlobe,
+      name: "Multilingual",
+      description: "Supports 100+ languages and counting.",
+      href: "/",
+      cta: "Learn more",
+      background: <img className="absolute -right-20 -top-20 opacity-60" />,
+      className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
+    },
+    {
+      Icon: CiGlobe,
+      name: "Multilingual",
+      description: "Supports 100+ languages and counting.",
+      href: "/",
+      cta: "Learn more",
+      background: <img className="absolute -right-20 -top-20 opacity-60" />,
+      className: "lg:col-start-3 lg:col-end-4 lg:row-start-3 lg:row-end-4",
+    },
+  ];
+
+  return (
+    <div className="relative">
+      <h1 className="text-center mt-10 text-3xl font-bold">Bento Grid</h1>
+
+      <div className="w-full h-full">
+
+        <BentoGrid className="lg:grid-rows-3">
+          {features.map((feature) => (
+            <BentoCard key={feature.name} {...feature} />
+          ))}
+        </BentoGrid>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
