adbab9eda7d6c714f88eed17913a9c52005150c5
rupeshpatil <rupeshpatil88927@gmail.com>
2025-09-07 23:12:11 +0530

    setup progress bar component

---
+++ progress-bar/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="relative">
+      <h1 className="text-center mt-10 text-3xl font-bold">Animated Circular Progress Bar</h1>
+
+    </div>
+  );
+}
+
+export default App;
