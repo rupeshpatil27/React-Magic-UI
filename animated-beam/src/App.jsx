303c6470f12cb29c9d5c26110b9957ec5fc10752
rupeshpatil <rupeshpatil88927@gmail.com>
2025-09-13 23:38:15 +0530

    initialize setup for animated beam component

---
+++ animated-beam/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className="relative">
+      <h1 className="text-center mt-10 text-3xl font-bold">Animated Beam</h1>
+
+    </div>
+  );
+}
+
+export default App;
+
+
