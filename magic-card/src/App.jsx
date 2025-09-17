5fdf0d141565d0efb1e37a127c9ec68b9d0fbe94
rupeshpatil <rupeshpatil88927@gmail.com>
2025-09-18 01:00:08 +0530

    Magic Card component - final update

--- magic-card/src/App.jsx
+++ magic-card/src/App.jsx
@@ -5,18 +5,19 @@ function App() {
     <div className="relative">
       <h1 className="text-center mt-10 text-3xl font-bold">Magic Card</h1>
 
-      <div className="w-[15rem] h-[60vh]">
-        <MagicCard
-          gradientColor={"#262626"}
-          className="p-0"
-          gradientFrom="red"
-          gradientTo="blue"
-        >
-          <div className="p-4">
-            <p>Hello World</p>
-            <span>Hover me</span>
-          </div>
-        </MagicCard>
+      <div className="w-full h-full flex flex-col gap-20 items-center mt-10">
+        <div className="p-0 w-[15rem] h-[50vh] shadow-none border-none rounded-xl">
+          <MagicCard
+            gradientColor="#D9D9D955"
+            className="p-0 w-full h-full flex items-center justify-center"
+            gradientFrom="#A07CFE"
+            gradientTo="#FE8FB5"
+          >
+            <div className="p-4">
+              <p>Hello World</p>
+            </div>
+          </MagicCard>
+        </div>
       </div>
     </div>
   );
